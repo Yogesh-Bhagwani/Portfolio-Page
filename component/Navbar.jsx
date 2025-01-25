dbffeffa4fee93a3e2cff787d9988f84568da461
Yogesh-Bhagwani <yogeshbhagwani05@gmail.com>
2025-01-25 14:02:47 +0530

    Add files

---
+++ component/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='w-full'>
+      <a href="">Home</a>
+      <a href="">About</a>
+      <a href="">Project</a>
+      <a href="">Experience</a>
+    </div>
+  )
+}
+
+export default Navbar
